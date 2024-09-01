d43e0131d740e33ea03dfe325a5f62e70ac02feb
vivit <jack.problems@protonmail.com>
2024-08-31 23:34:48 -0700

    teachers client build did not work. It says there are css errors, it will only display blank page with React title. Webclient section

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # nodejsTestDriven
+all course material can be found at 
+github.com/basarbk/tdd-nodejs
+npx http-server -c-1 -p 8080 -P http://localhost:3000
+
---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+ 
+app.listen(PORT, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", PORT);
+})
+
+// module.exports.handler = app
